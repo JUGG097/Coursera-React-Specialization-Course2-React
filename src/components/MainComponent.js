7adf36f32b9747c77f3b6655ffb06fd15dafc477
Jugg1 <adeoluwaadeboye@gmail.com>
2020-10-25 09:10:21 +0100

    Single Page Application Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -36,8 +36,16 @@ class Main extends Component {
         promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
         leader={this.state.leaders.filter((lead) => lead.featured)[0]}
         />
-      )
+      );
     }
+
+    const DishWithId = ({match}) => {
+      return (
+        <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]} 
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+      );
+    }
+
     return (
       <div>
         <Header/>
@@ -45,6 +53,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} /> } />
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
